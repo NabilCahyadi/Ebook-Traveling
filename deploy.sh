026e32ab38e29cd1d56547431f998df9b96a7b84
NabilCahyadi <nabilcahyadi155@gmail.com>
2025-12-09 10:39:43 +0700

    Add deploy.sh script for automated deployment

---
+++ deploy.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Masuk ke folder project
+cd /home/u778058510/domains/mappy.id/ebook_traveling_core
+
+# Update repo
+/usr/bin/git fetch --all
+/usr/bin/git reset --hard origin/main
+
+# Jalankan migrate fresh + seeder
+/usr/bin/php artisan migrate:fresh --seed
