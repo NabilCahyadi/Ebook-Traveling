e3803b2758bd209863944f40b737982755551728
NabilCahyadi <nabilcahyadi155@gmail.com>
2025-12-09 10:41:26 +0700

    Update deploy.sh script

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
 
-# Masuk ke folder project
-cd /home/u778058510/domains/mappy.id/ebook_traveling_core
+# Path ke project
+PROJECT_PATH="/home/u778058510/domains/mappy.id/ebook_traveling_core"
 
-# Update repo
+# Path ke PHP (ubah jika hosting berbeda)
+PHP_BIN="/usr/bin/php"
+
+# Masuk ke folder project
+cd $PROJECT_PATH
+
+# Update repo dari GitHub
 /usr/bin/git fetch --all
 /usr/bin/git reset --hard origin/main
 
-# Jalankan migrate fresh + seeder
-/usr/bin/php artisan migrate:fresh --seed
+# Jalankan migrate
+$PHP_BIN artisan migrate --force
+
+# Clear optimize (cache, config, view, route, dll)
+$PHP_BIN artisan optimize:clear
