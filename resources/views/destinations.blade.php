22ec07491e8e058c20cbf3e850d81b1459c0bd00
Kahla Luthfiyah Halim <kahla.luthfiyah22@smk.belajar.id>
2026-01-26 08:43:05 +0700

    perbaikan icon di destination

--- resources/views/destinations.blade.php
+++ resources/views/destinations.blade.php
@@ -311,10 +311,10 @@
         <!-- JS Anda membutuhkan tombol dengan class .next dan .prev -->
         <div class="slider-button">
             <button class="prev">
-                <i class="fi fi-rs-arrow-left"></i>
+                <i class="fi fi-rs-arrow-left mt-1"></i>
             </button>
             <button class="next">
-                <i class="fi fi-rs-arrow-right"></i>
+                <i class="fi fi-rs-arrow-right mt-1"></i>
             </button>
         </div>
     </div>
